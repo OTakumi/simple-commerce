71e4eb0e9765e007ad1ecc336b8a5a058bee014b
OTakumi <takumi.o.0410@gmail.com>
2022-11-20 21:28:10 +0900

    Update style theme settings

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,10 @@
+import {addDecorator} from '@storybook/react'
+import {createGlobalStyle, ThemeProvider} from 'styled-components'
+import {theme} from '../src/themes'
+import * as NextImage from 'next/image'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {argTypesRegex: "^on[A-Z].*"},
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -7,3 +12,45 @@ export const parameters = {
     },
   },
 }
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+	  padding: 0;
+	  margin: 0;
+	  font-family: -apple-system, BlinkMaeSystemFont, Segoe UI, Roboto, Oxygen, 
+		  Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  * {
+	  box-sizing: border-box;
+  }
+
+  a {
+	  cursor: pointer;
+	  text-decoration: none;
+	  transition: .25s;
+	  color: #000;
+  }
+`
+
+// Theme
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {story()}
+  </ThemeProvider>
+))
+
+// replace next/image
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => typeof props.src === 'string' ? (
+    <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+  ) : (
+    <OriginalNextImage {...props} unoptimized />
+  ),
+})
