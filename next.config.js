2416d41f4cfb02df8411957891367b03a148dcc5
OTakumi <takumi.o.0410@gmail.com>
2022-11-23 23:44:23 +0900

    Added API request proxy configuration for development environment

--- next.config.js
+++ next.config.js
@@ -2,10 +2,31 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-	compiler: {
-		// enable styleComponets
-		styledComponents: true
-	},
+  compiler: (() => {
+    let compilerConfig = {
+      // enable styleComponets
+      styledComponents: true
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // Remove the data-testid attribute used by React Testing Library in the production env.
+        reactRemoveProperties: {properties: ['^data-testingid$'], }
+      }
+    }
+    return compilerConfig
+  })(),
+  async rewrites() {
+    return [
+      {
+        // ex ./app/proxy
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        // ex. http://localhost:8000
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
